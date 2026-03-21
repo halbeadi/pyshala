0279216cd0bda85f487a399429cfac6ebf5d8b95
Aditya halbe <32957277+halbeadi@users.noreply.github.com>
2026-03-21 09:20:46 +0000

    Added accounts - signup, login, dashboard working

--- pyshala/accounts/urls.py
+++ pyshala/accounts/urls.py
@@ -1,2 +1,9 @@
 from django.urls import path
-urlpatterns = []
+from . import views
+
+urlpatterns = [
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+]
