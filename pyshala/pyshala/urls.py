2d9de5e043217e38556d757e6c478b4896fc25db
Aditya halbe <32957277+halbeadi@users.noreply.github.com>
2026-03-23 06:53:44 +0000

    Added About page

--- pyshala/pyshala/urls.py
+++ pyshala/pyshala/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('accounts/', include('accounts.urls')),
     path('courses/', include('courses.urls')),
     path('interpreter/', include('interpreter.urls')),
